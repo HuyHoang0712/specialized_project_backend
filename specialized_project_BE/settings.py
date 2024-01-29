036c52cd9b7e022275d62db9a74250e3c0554706
HuyHoang0712 <hoangdinhhuy0712@gmail.com>
2024-01-29 14:17:19 +0700

    update

--- specialized_project_BE/settings.py
+++ specialized_project_BE/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": "logistic",
         "USER": "postgres",
-        "PASSWORD": "password",
+        "PASSWORD": "Huy071202",
         "HOST": "localhost",
         "PORT": "5432",
     }
