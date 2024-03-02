74ba1e4f2333d7b72c2fe965cf5fb3ede8bf84a2
Vũ Việt Hưng <vuviethungnbee@gmail.com>
2024-03-02 15:42:22 +0700

    Dashboard API code

--- api/views/issue_view.py
+++ api/views/issue_view.py
@@ -1,9 +1,38 @@
 from api.models import Issue
 from api.serializers import IssueSerializer
 from .backend import *
+from datetime import datetime, timedelta
+
+
+start = datetime.today().now().replace(hour=0, minute=0, second=0, microsecond=0)
+end = datetime.today().now().replace(
+    hour=0, minute=0, second=0, microsecond=0
+) + timedelta(days=1, microseconds=-1)
 
 
 class IssueViewSet(viewsets.ModelViewSet):
     queryset = Issue.objects.all()
     serializer_class = IssueSerializer
     authentication_classes = (JWTAuthentication,)
+
+    @action(detail=False, methods=["GET"])
+    def get_issues_by_today(self, request, pk=None):
+        print(end)
+        q1 = Issue.objects.filter(date_time__range=(start, end))
+        data = {}
+        count = 0
+        for x in q1:
+            res = {
+                "title": x.title,
+                "description": x.description,
+                "date_time": x.date_time,
+                "status": x.status,
+                "label": x.label,
+                "creator_id": x.creator_id,
+                "order_id": x.order_id,
+                "vehicle_license_plate": x.vehicle_license_plate,
+                "warehouse_id": x.warehouse_id,
+            }
+            data[count] = res
+            count += 1
+        return Response(data, status=status.HTTP_200_OK)
--- api/views/order_view.py
+++ api/views/order_view.py
@@ -1,9 +1,30 @@
 from api.models import Order
 from api.serializers import OrderSerializer
 from .backend import *
+from datetime import datetime
+
+today = datetime.today().strftime("%Y-%m-%d")
 
 
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
     authentication_classes = (JWTAuthentication,)
+
+    @action(detail=False, methods=["GET"])
+    def get_order_by_today(self, request, pk=None):
+        q1 = Order.objects.filter(date=today)
+        data = {}
+        count = 0
+        for x in q1:
+            res = {
+                "ship_code": x.ship_code,
+                "date": x.date,
+                "time_in": x.time_in,
+                "payload": x.payload,
+                "pickup_id": x.pickup_id,
+                "employee_id": x.employee_id,
+            }
+            data[count] = res
+            count += 1
+        return Response(data, status=status.HTTP_200_OK)
