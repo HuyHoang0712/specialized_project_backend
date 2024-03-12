206ba90ee985dbe01584dc0b22d4fb1d914d87a9
HuyHoang0712 <hoangdinhhuy0712@gmail.com>
2024-03-12 12:28:56 +0700

    Update admin.py

--- api/admin.py
+++ api/admin.py
@@ -3,57 +3,11 @@ from .models import *
 
 
 # Register your models here.
-@admin.register(Employee)
-class EmployeeAdmin(admin.ModelAdmin):
-    list_display = ["id", "name", "role", "status"]
-
-
-@admin.register(Warehouse)
-class WarehouseAdmin(admin.ModelAdmin):
-    list_display = ["id", "name", "address", "longitude", "latitude"]
-
-
-@admin.register(TransportationPlan)
-class TransportationPlan(admin.ModelAdmin):
-    list_display = ["id", "date"]
-
-
-@admin.register(TransportationPlan)
-class TransportationPlan(admin.ModelAdmin):
-    list_display = ["id", "date"]
-
-
+admin.site.register(Employee)
+admin.site.register(Warehouse)
+admin.site.register(TransportationPlan)
 admin.site.register(Notification)
-
-
-@admin.register(Vehicle)
-class Vehicle(admin.ModelAdmin):
-    list_display = [
-        "license_plate",
-        "capacity",
-        "fuel_consumption_level",
-        "status",
-        "driver_id",
-    ]
-
-
-@admin.register(Order)
-class Order(admin.ModelAdmin):
-    list_display = [
-        "id",
-        "ship_code",
-        "date",
-        "time_in",
-        "payload",
-        "pickup_id",
-        "employee_id",
-        "plan_id",
-    ]
-
-
-@admin.register(DeliveryPoint)
-class DeliveryPoint(admin.ModelAdmin):
-    list_display = ["name", "address", "longitude", "latitude"]
-
-
+admin.site.register(Vehicle)
+admin.site.register(Customer)
+admin.site.register(Order)
 admin.site.register(Issue)
