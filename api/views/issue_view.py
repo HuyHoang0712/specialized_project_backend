3308600f4bbbdd4225afb9c7a1850641c3c6cb88
HuyHoang0712 <hoangdinhhuy0712@gmail.com>
2024-03-13 10:23:44 +0700

    refactor: getAllIssuesApi

--- api/views/issue_view.py
+++ api/views/issue_view.py
@@ -9,39 +9,13 @@ end = datetime.today().now().replace(
 
 
 class IssueViewSet(viewsets.ModelViewSet):
-    queryset = Issue.objects.all()
+    queryset = Issue.objects.all().order_by("-date_time").values()
     serializer_class = IssueSerializer
     authentication_classes = (JWTAuthentication,)
 
-    @action(detail=False, methods=["GET"])
-    def get_issues_by_today(self, request, pk=None):
-        issues = Issue.objects.filter(date_time__range=(start, end))
-        creator_id = ""
-        order_id = ""
-        vehicle_license_plate = ""
-        warehouse_id = ""
-        data = []
-        for issue in issues:
-            # Handle null data
-            if issue.creator_id:
-                creator_id = issue.creator_id.id
-            if issue.order_id:
-                order_id = issue.order_id.id
-            if issue.vehicle_license_plate:
-                vehicle_license_plate = issue.vehicle_license_plate.license_plate
-            if issue.warehouse_id:
-                warehouse_id = issue.warehouse_id.id
-            # Return issue object
-            res = {
-                "title": issue.title,
-                "description": issue.description,
-                "date_time": issue.date_time,
-                "status": issue.status,
-                "label": issue.label,
-                "creator_id": creator_id,
-                "order_id": order_id,
-                "vehicle_license_plate": vehicle_license_plate,
-                "warehouse_id": warehouse_id,
-            }
-            data.append(res)
-        return Response(data, status=status.HTTP_200_OK)
+    @action(detail=False, methods=["post"])
+    def get_issues_by_date(self, request, pk=None):
+        qr_date = request.data["date"]
+        queryset = Issue.objects.filter(date_time=qr_date)
+        serializer = IssueSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
