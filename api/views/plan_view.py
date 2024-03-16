4d1e38cce85dcc5f803173a82047f574498bc10d
HuyHoang0712 <hoangdinhhuy0712@gmail.com>
2024-03-16 14:13:08 +0700

    refactor: import file_upload api

--- api/views/plan_view.py
+++ api/views/plan_view.py
@@ -12,6 +12,7 @@ class PlanViewSet(viewsets.ModelViewSet):
     queryset = TransportationPlan.objects.all()
     serializer_class = TransportationPlanSerializer
     authentication_classes = (JWTAuthentication,)
+
     # authentication_classes = ()
     # permission_classes = ()
 
@@ -24,51 +25,22 @@ class PlanViewSet(viewsets.ModelViewSet):
     @action(detail=False, methods=["post"])
     @parser_classes([FileUploadParser])
     def file_upload(self, request):
-
         data = request.data["file"]
-        reader = pd.read_excel(
-            data,
-            sheet_name=0,
-            header=2,
-        )
-        # print(reader.loc[0])
-        customers = []
-        unknow_customers = []
+        reader = pd.read_excel(data, sheet_name=0, header=2)
         label_index = [0, 1, 4, 16]
-        for i in range(0, reader.shape[0]):
-            customer = {
-                "ship_code": "",
-                "contact_name": "",
-                "longtitude": "",
-                "latitude": "",
-                "order_type": "",
-                "total_tons": "",
-            }
-            count = 0
-            for i1 in label_index:
-                if count == 0:
-                    customer["ship_code"] = reader.iloc[i, i1]
-                elif count == 1:
-                    customer["contact_name"] = reader.iloc[i, i1]
-                elif count == 2:
-                    customer["order_type"] = reader.iloc[i, i1]
-                elif count == 3:
-                    customer["total_tons"] = math.ceil(reader.iloc[i, i1] * 1000)
-                count += 1
-            qr_contact_name = customer["contact_name"]
-            queryset = Customer.objects.filter(
-                name__unaccent__icontains=qr_contact_name.strip()
-            ).values()
-            for item in queryset:
-                if qr_contact_name.replace(" ", "") == item["name"].replace(" ", ""):
-                    # print(True)
-                    customer["longtitude"] = item["longitude"]
-                    customer["latitude"] = item["latitude"]
-                else:
-                    unknow_customers.append(qr_contact_name)
-            customers.append(customer)
-        if len(unknow_customers) != 0:
-            res = []
-            [res.append(x) for x in unknow_customers if x not in res]
-            return Response(res, status=status.HTTP_204_NO_CONTENT)
+        labels = ["ship_code", "contact_name", "order_type", "total_tons"]
+        customers = []
+        unknow_customers = set()
+
+        for i in range(reader.shape[0]):
+            customer = {label: reader.iloc[i, idx] if idx != 16 else math.ceil(reader.iloc[i, idx] * 1000) for
+                        label, idx in zip(labels, label_index)}
+            queryset = Customer.objects.filter(name__unaccent__icontains=customer["contact_name"].strip()).values()
+            if queryset:
+                unknow_customers.add(customer["contact_name"].strip())
+            else:
+                customers.append(customer)
+
+        if unknow_customers:
+            return Response(list(unknow_customers), status=status.HTTP_204_NO_CONTENT)
         return Response(customers, status=status.HTTP_200_OK)
