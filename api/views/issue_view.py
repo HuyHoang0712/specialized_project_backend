c85ebfc5578754c87240d6c2f9614ff1f3d03038
Vũ Việt Hưng <vuviethungnbee@gmail.com>
2024-04-02 20:44:54 +0700

    Update issue_view.py

--- api/views/issue_view.py
+++ api/views/issue_view.py
@@ -26,3 +26,15 @@ class IssueViewSet(viewsets.ModelViewSet):
         queryset = Issue.objects.filter(status=qr_status).order_by("-date_time")
         serializer = IssueSerializer(queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    @action(detail=False, methods=["get"])
+    def get_user_issues(self, request, pk=None):
+        account_id = request.user.id
+        print(request.user.id)
+        profile = Employee.objects.filter(user=account_id)
+        profile_serializer = EmployeeSerializer(profile, many=True)
+        employee_id = profile_serializer.data[0]["id"]
+        print(profile_serializer.data[0]["id"])
+        issues = Issue.objects.filter(creator=employee_id)
+        issue_serializer = IssueSerializer(issues, many=True)
+        return Response(issue_serializer.data, status=status.HTTP_200_OK)
