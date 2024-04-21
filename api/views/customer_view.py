9d24eff98e973782e3785db5fd03644d8213d636
Vũ Việt Hưng <vuviethungnbee@gmail.com>
2024-04-21 22:48:07 +0700

    Update customer_view.py

--- api/views/customer_view.py
+++ api/views/customer_view.py
@@ -2,18 +2,21 @@ from .backend import *
 from api.utils.get_location_service.get_location import *
 
 
-
 class CustomerViewSet(viewsets.ModelViewSet):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
     authentication_classes = (JWTAuthentication,)
     permission_classes = (IsAuthenticated,)
 
-    @action(detail=False, methods=["post"])
+    @action(
+        detail=False,
+        methods=["post"],
+        permission_classes=[
+            permission_required("api.add_customer", raise_exception=True)
+        ],
+    )
     def create_customer(self, request):
         # Check permission
-        if not request.user.has_perm("api.add_customer"):
-            return Response("You don't have permission to create Customer", status=status.HTTP_400_BAD_REQUEST)
         data = request.data
         print(data)
         if data["address"]:
@@ -34,20 +37,27 @@ class CustomerViewSet(viewsets.ModelViewSet):
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    @action(detail=False, methods=["get"])
+    @action(
+        detail=False,
+        methods=["get"],
+        permission_classes=[
+            permission_required("api.view_customer", raise_exception=False)
+        ],
+    )
     def get_customer_by_id(self, request):
-        if not request.user.has_perm("api.view_customer"):
-            return Response("You don't have permission to view Customer", status=status.HTTP_400_BAD_REQUEST)
         qr_id = request.query_params["id"]
         queryset = Customer.objects.get(id=qr_id)
         serializer = CustomerSerializer(queryset)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-
-    @action(detail=False, methods=["put"])
+    @action(
+        detail=False,
+        methods=["put"],
+        permission_classes=[
+            permission_required("api.change_customer", raise_exception=False)
+        ],
+    )
     def update_customer(self, request):
-        if not request.user.has_perm("api.change_customer"):
-            return Response("You don't have permission to update Customer", status=status.HTTP_400_BAD_REQUEST)
         customer_id = request.query_params["id"]
         data = request.data
         if "address" in data.keys():
