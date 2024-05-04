a35c38b4b1ead8f48786c89ff5eb7bd7f3b360c3
HuyHoang0712 <hoangdinhhuy0712@gmail.com>
2024-05-04 12:09:38 +0700

    update .gitignore

--- api/migrations/0005_remove_employee_role_employee_phone_issue_and_more.py
+++ api/migrations/0005_remove_employee_role_employee_phone_issue_and_more.py
@@ -6,9 +6,6 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
-    dependencies = [
-        ('api', '0004_remove_requests_vehicle_request_id_and_more'),
-    ]
 
     operations = [
         migrations.CreateModel(
